bf2b9c53cb06071766941b3cc45673dcfc6d8c1d
Alexey Subbotin <subbotin910@gmail.com>
2018-09-23 13:18:40 +0700

    const method

--- subb_lab1/subb_lab1/Program.cs
+++ subb_lab1/subb_lab1/Program.cs
@@ -11,19 +11,71 @@ namespace subb_lab1
     {
         static void Main(string[] args)
         {
+            
+        }
+
+        public static void ParseFile()
+        {
             StreamReader sr = new StreamReader("input.txt");
 
             string input = sr.ReadLine();
 
-            while(input != "")
+            while (input != "")
             {
                 string[] inputArr = input.Split(' ');
 
-                if(inputArr[0] == "const")
+                if (inputArr[0] == "const")
                 {
 
                 }
             }
+        }
+
+        private static Identifier CreateConst(string input)
+        {
+            Const newConst = new Const();
+
+            string[] inputArr = input.Split(' ');
+
+            bool knownType = false;
+
+            switch (inputArr[1])
+            {
+                case "int":
+                    newConst.IdentType = Identifier.IdentTypes.int_type;
+                    knownType = true;
+                    break;
+
+                case "float":
+                    newConst.IdentType = Identifier.IdentTypes.float_type;
+                    knownType = true;
+                    break;
+
+                case "bool":
+                    newConst.IdentType = Identifier.IdentTypes.bool_type;
+                    knownType = true;
+                    break;
+
+                case "char":
+                    newConst.IdentType = Identifier.IdentTypes.char_type;
+                    knownType = true;
+                    break;
+
+                case "string":
+                    newConst.IdentType = Identifier.IdentTypes.string_type;
+                    knownType = true;
+                    break;
+            }
+
+            if (!knownType)
+                newConst.IdentType = Identifier.IdentTypes.class_type;
+
+            newConst.Name = inputArr[2];
+
+            if (inputArr[3] != "" && inputArr[3] == "=")
+                newConst.Value = inputArr[4];
+
+            return newConst;
         }
     }
 }
